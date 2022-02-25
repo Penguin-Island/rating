1dc06452224481919f2328899375a4bdf177d34f
Penguin-Island <penguinisland.dev@gmail.com>
2022-02-25 22:36:58 +0900

    change state to clicked, Remove unused code

--- rating.js
+++ rating.js
@@ -1,10 +1,10 @@
 var stars = document.getElementsByClassName("star");
-var state = false;
-document.addEventListener("DOMContentLoaded", function () {
+var clicked = false;
+document.addEventListener("DOMContentLoaded", () => {
     console.log(state);
     for (let i = 0; i < stars.length; i++) {
         stars[i].addEventListener("mouseover", () => {
-            if (!state) {
+            if (!clicked) {
                 console.log(state);
                 for (let j = 0; j <= i; j++) {
                     stars[j].style.color = "#f0da61";
@@ -13,24 +13,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }, false);
 
         stars[i].addEventListener("mouseout", () => {
-            if (!state) {
+            if (!clicked) {
                 console.log(state);
                 for (let j = 0; j < stars.length; j++) {
                     stars[j].style.color = "#a09a9a";
                 }
             }
         }, false);
-    }
 
-    for (let i = 0; i < stars.length; i++) {
         stars[i].addEventListener("click", () => {
-            state = true;
+            clicked = true;
             console.log(state);
             for (let j = 0; j <= i; j++) {
                 stars[j].style.color = "#f0da61";
             }
-            for (let k = i + 1; k < stars.length; k++) {
-                stars[k].style.color = "#a09a9a";
+            for (let j = i + 1; j < stars.length; j++) {
+                stars[j].style.color = "#a09a9a";
             }
         }, false);
     }
