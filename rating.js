d51c5c9a03943d89876bbd70107beb8d54339f66
Penguin-Island <penguinisland.dev@gmail.com>
2022-02-25 22:37:41 +0900

    Remove unused code

--- rating.js
+++ rating.js
@@ -1,11 +1,9 @@
 var stars = document.getElementsByClassName("star");
 var clicked = false;
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(state);
     for (let i = 0; i < stars.length; i++) {
         stars[i].addEventListener("mouseover", () => {
             if (!clicked) {
-                console.log(state);
                 for (let j = 0; j <= i; j++) {
                     stars[j].style.color = "#f0da61";
                 }
@@ -14,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         stars[i].addEventListener("mouseout", () => {
             if (!clicked) {
-                console.log(state);
                 for (let j = 0; j < stars.length; j++) {
                     stars[j].style.color = "#a09a9a";
                 }
@@ -23,7 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         stars[i].addEventListener("click", () => {
             clicked = true;
-            console.log(state);
             for (let j = 0; j <= i; j++) {
                 stars[j].style.color = "#f0da61";
             }
